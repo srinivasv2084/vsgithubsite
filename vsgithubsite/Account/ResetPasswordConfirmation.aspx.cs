d17c1a2c69d6dd1839dcafcc3d96f49a05ab0010
srinivasv2084 <srinivasv2084@outlook.com>
2014-05-08 15:41:07 +0530

    FirstCommit

---
+++ vsgithubsite/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace vsgithubsite.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ vsgithubsite/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(vsgithubsite.Startup))]
+namespace vsgithubsite
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
